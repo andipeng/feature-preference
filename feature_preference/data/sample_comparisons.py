d3e47d694a2d3cfa0c13872c080f786768aebb26
Andi Peng <andipeng@Andis-MacBook-Pro.local>
2023-08-24 01:20:18 -0400

    rlhf

---
+++ feature_preference/data/sample_comparisons.py
@@ -0,0 +1,93 @@
+import os
+import numpy as np
+import argparse
+import pickle
+import random
+import csv
+import itertools
+
+##### Define features and actions
+#TEXTURES = ['spotted', 'plain', 'striped']
+#COLORS = ['green', 'red', 'blue']
+#SHAPES = ['circle', 'square', 'triangle']
+#HEIGHTS = ['tall', 'medium', 'short']
+#WEIGHTS = ['heavy', 'middle', 'light']
+#SMELLS = ['sweet', 'earthy', 'rancid']
+TEXTURES = [[1,0,0],[0,1,0],[0,0,1]]
+COLORS = [[1,0,0],[0,1,0],[0,0,1]]
+SHAPES = [[1,0,0],[0,1,0],[0,0,1]]
+HEIGHTS = [[1,0,0],[0,1,0],[0,0,1]]
+WEIGHTS = [[1,0,0],[0,1,0],[0,0,1]]
+SMELLS = [[1,0,0],[0,1,0],[0,0,1]]
+FEATURES = TEXTURES + COLORS + SHAPES + HEIGHTS + WEIGHTS + SMELLS
+
+# Define all possible states
+STATES = [{"texture": state[0], "color": state[1], "shape": state[2], "height": state[3], "weight": state[4], "smell": state[5]} 
+           for state in itertools.product(TEXTURES, COLORS, SHAPES, HEIGHTS, WEIGHTS, SMELLS)]
+
+#TRUE_REWARDS = {"spotted": 1, "plain": 0,  "striped": -1, 
+#                "green": 1, "red": 0, "blue": -1,
+#                "circle": 1, "square": 0, "triangle": -1,
+#                "tall": 0, "medium": 0, "short": 0,
+#                "heavy": 0, "triangle": 0, "square": -1,
+#                "sweet": 0, "earthy": 0, "rancid": 0}
+TRUE_REWARDS = {"texture":[1,0,-1],
+                "color":[1,0,-1],
+                "shape":[1,0,-1],
+                "height":[0,0,0],
+                "weight":[0,0,0],
+                "smell":[0,0,0]}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_comparisons', type=int, default=300)
+parser.add_argument('--save_file', type=str, default='train_rewards300.csv')
+
+args = parser.parse_args()
+
+# turns dict state into flat state
+def flatten_state(states):
+    flattened_state = []
+    for d in states:
+        for value in d.values():
+            flattened_state.append(value)
+    return np.array(flattened_state).flatten()
+
+# calculates state reward based on ground truth rewards
+def calculate_reward(state):
+    rewards = 0
+    for feature in state.keys():
+        index = state[feature].index(1)
+        reward = TRUE_REWARDS[feature][index]
+        rewards+=reward
+    return rewards
+
+print("========================================")
+print("Sampling %d comparisons" % args.num_comparisons)
+print("========================================")
+
+# samples random mushroom from all possible features
+
+with open(args.save_file, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    
+    for sample in range(args.num_comparisons):
+        final_list = []
+        # samples 2 states and obtains rewards
+        state1 = random.sample(STATES, 1)
+        state2 = random.sample(STATES, 1)
+        state1_reward = calculate_reward(state1[0])
+        state2_reward = calculate_reward(state2[0])
+        # flattens for writing
+        state1 = flatten_state(state1)
+        state2 = flatten_state(state2)
+        # saves preferences
+        pref = 0
+        if state1_reward > state2_reward:
+            pref = 1
+
+        final_list.extend(state1)
+        final_list.extend([state1_reward])
+        final_list.extend(state2)
+        final_list.extend([state2_reward])
+        final_list.extend([pref])
+        csv_writer.writerow(final_list)
